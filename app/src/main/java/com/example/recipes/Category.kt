f3189adccddac46e91710358fd94271d7fcc4ec1
rajatjain20303 <rajatjain626@gmail.com>
2024-01-23 01:54:10 +0530

    Learning how to use APIs and Connecting app to internet(Learning).

---
+++ app/src/main/java/com/example/recipes/Category.kt
@@ -0,0 +1,8 @@
+package com.example.recipes
+
+data class Category(val idCategory:String,
+      val strCategory:String,
+      val strCategoryThumb: String,
+      val strCategoryDescription:String)
+
+data class CategoriesResponse(val categories:List<Category>)
